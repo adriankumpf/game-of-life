2c90e05bc819a6c6fd1837bb7d5a810027d89f88
Adrian Kumpf <adrian.kumpf@posteo.de>
2017-04-16 22:53:16 +0200

    Draw cells at every tick

--- apps/game/lib/game.ex
+++ apps/game/lib/game.ex
@@ -1,18 +1,44 @@
 defmodule Game do
-  @moduledoc """
-  Documentation for Game.
-  """
 
-  @doc """
-  Hello world.
+  @width 60
+  @height 40
+  @rate 250
 
-  ## Examples
+  def start do
+    {:ok, pid} = Game.Universe.init(@width, @height)
+    :ok = randomly_place_cells(pid)
+    loop(pid)
+  end
 
-      iex> Game.hello
-      :world
+  def randomly_place_cells(pid) do
+    for _ <- 0..trunc(@width*@height*0.45) do
+      x = :rand.uniform(@width) - 1
+      y = :rand.uniform(@height) - 1
+      Game.Universe.create_cell(pid, {x, y})
+    end
+    :ok
+  end
 
-  """
-  def hello do
-    :world
+  def draw(cells) do
+    for y <- 0..@height-1 do
+      for x <- 0..@width-1  do
+        cells[{x, y}]
+        |> to_symbol
+        |> String.pad_leading(3)
+        |> IO.write
+      end
+      IO.puts ''
+    end
+    IO.puts ''
+  end
+
+  defp to_symbol(1), do: "O"
+  defp to_symbol(_), do: "-"
+
+  def loop(pid) do
+    Game.Universe.tick(pid)
+    Game.Universe.get_cells(pid) |> draw
+    :timer.sleep(@rate)
+    loop(pid)
   end
 end
